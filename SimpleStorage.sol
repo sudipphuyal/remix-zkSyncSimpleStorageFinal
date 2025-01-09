2655e955c9587601c6fc1d625f0d696969a3bcf3
sudipphuyal <sudip.phuyal@iscte-iul.pt>
2025-01-09 00:16:40 +0000

    following patrick collins

---
+++ SimpleStorage.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;  //this is solidity version
+
+contract SimpleStorage{
+    // basic types: boolean, uint, int, address, bytes
+    // bool hasFavouriteNumber = true;
+//   uint256 favNumber = 88;
+// int256 favNum = -88;
+// string favNuminText = "88";
+// address myAddress = 0x5A93230aDc3c36Cff829ca7bD0816A3d3503B383;
+// bytes32 favBytes = "cat"; //0x2sdasa
+
+// bytes ffds 
+
+//bytes32 
+//uint256 favNumber;  //0
+
+
+uint 
+
+
+
+
+}
